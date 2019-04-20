6b69992501e8e51afd1ad7fd7697c6313ae1e8e3
Brittany Bowman <britpetty@gmail.com>
2019-04-20 14:56:38 -0600

    Completed/Reviewed 4/20/19

--- practice_answers.js
+++ practice_answers.js
@@ -1,3 +1,11 @@
+/* 
+  Once you complete a problem, refresh ./SpecRunner.html in your browser and check to see if the problem's test(s) are passing.
+  Passed tests will be indicated by a green circle.
+  Failed tests will be indicated by a red X.
+
+  You can refresh the page at any time to re-run all the tests.
+*/
+
 ////////// PROBLEM 1 //////////
 
 // Do not edit the code below.
@@ -32,7 +40,7 @@ function last(arr) {
   return arr.slice(-1).pop();
 }
 last(arr);
-
+//s
 ////////// PROBLEM 3 //////////
 
 // Do not edit the code below.
